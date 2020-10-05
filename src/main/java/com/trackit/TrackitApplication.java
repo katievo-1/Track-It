ca4799580f488610384f50fb60d3fa54a9d436b5
Administrator <Administrator@DESKTOP-099UQRG>
2020-10-05 17:09:21 -0400

    Initial Commit of Project with Spring Boot

---
+++ src/main/java/com/trackit/TrackitApplication.java
@@ -0,0 +1,13 @@
+package com.trackit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrackitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trackit/TrackitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trackit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
