e9dc863bd8c1f899fcebf9e1c33bda340aa02591
Administrator <Administrator@DESKTOP-099UQRG>
2020-10-22 15:08:15 -0400

    Create java class to implement grabbing food data and storing into array (code unfinished due to not having a DB yet)

--- src/main/java/com/trackit/Service/TrackitService.java
+++ src/main/java/com/trackit/Service/TrackitService.java
@@ -1,7 +1,13 @@
 package com.trackit.Service;
 
+import com.trackit.trackitDTO;
+
 public interface TrackitService {
 
 	
+	
+	trackitDTO fetchbyID(String foodName);
+	
+	void save(trackitDTO trackitdto);
 	
 }
---
+++ src/main/java/trackit/DAO/FoodArray.java
@@ -0,0 +1,8 @@
+package trackit.DAO;
+
+public class FoodArray {
+
+	
+	
+	
+}
