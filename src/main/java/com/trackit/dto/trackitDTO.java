ea111ef0e29b8892555cde44cb5ad2dba5f9958e
Administrator <Administrator@DESKTOP-099UQRG>
2020-11-02 16:11:51 -0500

    added @table name and @Entity to DTO package linking it too future database

--- src/main/java/com/trackit/dto/trackitDTO.java
+++ src/main/java/com/trackit/dto/trackitDTO.java
@@ -1,14 +1,44 @@
 package com.trackit.dto;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity //maps class to database table
+@Table(name="Foods")
 public class trackitDTO {
 
 	
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	
+	@Column(name="Food_id")
+	private int id;
+	
+	@Column(name="food_Name")
 	private String foodName;
+	
+	@Column(name="Calories")
 	private int calories;
+	
+	@Column(name="serving_Size")
 	private int servingSize;
+	
+	//Set Constructors
+	
+	
+	public trackitDTO() {
+		
+	}
+	
 	
 	
 	public trackitDTO(String foodName, int calories, int servingSize) {
+		super();
 		this.foodName = foodName;
 		this.calories = calories;
 		this.servingSize = servingSize;
